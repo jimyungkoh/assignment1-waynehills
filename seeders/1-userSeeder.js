dea0eccffc9524397289f5005dc036077cec3b77
dingwan <dingwan0331@gmail.com>
2022-09-01 21:30:16 +0900

    create: 프로젝트 모델링, 시더파일 생성
    - app.js에 sequlize sync 작성
    - userModel & postModel 작성
    - underscored: true를 사용하여 DB상 칼럼명은 snake_case로 작성
    - mysql2 모듈 import
    - 회원비밀번호 암호화에 필요한 bcrypt 모듈 import
    - userSeeders & postSeeder 작성

---
+++ seeders/1-userSeeder.js
@@ -0,0 +1,81 @@
+"use strict";
+
+const bcrypt = require("bcrypt");
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const now = new Date();
+    const saltRounds = 16;
+    const salt = await bcrypt.genSalt(saltRounds);
+
+    // 어드민 계정 생성
+    await queryInterface.bulkInsert(
+      "users",
+      [
+        {
+          name: "관리자1",
+          username: "admin1",
+          password: await bcrypt.hash("admin123!", salt),
+          phone_number: "010-1234-1234",
+          role: "admin",
+          gender: "mail",
+          birthday: "1999-01-17",
+          last_login_date: "2022-01-17T04:33:12.000",
+          created_at: now,
+          updated_at: now,
+        },
+        {
+          name: "관리자2",
+          username: "admin2",
+          password: await bcrypt.hash("admin123@", salt),
+          phone_number: "010-1234-5678",
+          role: "admin",
+          gender: "femail",
+          birthday: "2000-03-17",
+          last_login_date: "2021-06-11T04:33:12.000",
+          created_at: now,
+          updated_at: now,
+        },
+      ],
+      {}
+    );
+
+    // 일반유저 계정 생성
+    await queryInterface.bulkInsert(
+      "users",
+      [
+        {
+          name: "유저1",
+          username: "user1",
+          password: await bcrypt.hash("user123!", salt),
+          phone_number: "010-1111-2222",
+          role: "user",
+          gender: "mail",
+          birthday: "1988-07-19",
+          last_login_date: "2022-07-20T04:33:12.000",
+          created_at: now,
+          updated_at: now,
+        },
+        {
+          name: "유저2",
+          username: "user2",
+          password: await bcrypt.hash("user123@", salt),
+          phone_number: "010-3333-4444",
+          role: "user",
+          gender: "femail",
+          birthday: "1994-03-11",
+          last_login_date: "2022-04-30T04:33:12.000",
+          created_at: now,
+          updated_at: now,
+        },
+      ],
+      {}
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    const userNames = ["user1", "user2", "admin1", "admin2"];
+
+    await queryInterface.bulkDelete("users", { username: userNames }, {});
+  },
+};
