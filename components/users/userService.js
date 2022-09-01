93eafbf2b34f39348689ad5ae97f2ab6485503ca
서유진 <50348197+yujiniii@users.noreply.github.com>
2022-09-02 03:12:58 +0900

    feat: jwt 를 사용한 로그인 구현(중)
    jwt ;  사용자 토큰을 만들어주는 메서드verify ; 해당 토큰을 사용자 id로 해석해주는 메서드
    findUser; 사용자가 있는지, 사용자가 비밀번호를 잘 입력했는지를 확인하는 메서드

--- components/users/userService.js
+++ components/users/userService.js
@@ -12,6 +12,58 @@ hashPassword = async (password) => {
     const hash = await bcrypt.hash(password, salt)
     return hash;
 }
+/**
+ * jwt token 을 만드는 메서드 : jwt
+ * @param {int} id 
+ * @returns {}
+ */
+const jwt = async (userId) =>{
+    const payload = {
+        id : userId
+    }
+    const result = {
+        token : jwt.sign(payload, secretKey, option)
+    }
+    return result;
+}
+/**
+ * 토큰을 통해 다시 사용자id를 불러오는 메서드 : verify
+ * @param {string} token 
+ * @returns {int} 
+ */
+const verify = async (token) => {
+    let decoded;
+    try {
+      decoded = await jwt.verify(token, secretKey, option);
+    } catch (err) {
+        if (err.message === 'jwt expired') {
+            console.log('expired token');
+            return 'TOKEN_EXPIRED';
+        } else {
+            console.log('invalid token');
+            return 'TOKEN_INVALID';
+        }
+    }
+
+    return decoded;
+}
+
+/**
+ * 테이블에 username과 password가 같은 항목을 찾는 메서드 : findUser
+ * @param {string} username 
+ * @param {string} password 
+ * @returns 
+ */
+const findUser = async (username, password) => {
+    return await User.findOne({
+        where: {
+            username: username,
+            password: hashPassword(password)}
+    }).catch((err) => {
+        return next(err);
+    });
+}
+
 
 
 /**
@@ -41,6 +93,33 @@ exports.signUp = async (name, username, birthday, gender, phoneNumber,  password
         return BadRequestError(err)
     });
     return res.status(200).json({newUser})
+}
+
+/**
+ * @todo 회원 로그인 login 메서드
+ * @param {string} username 
+ * @param {string} password
+ * @returns {???} // 제 생각은 없어도 될듯 합니다만,,
+ */ 
+// login 성공시 req에 username과 role을 추가해주세요.
+// ex))
+// req.username = username;
+// req.role = 'admin'
+exports.login = async (username, password) => {
+    const correctUser = await findUser(username, password);
+
+    if(correctUser){
+        const jwtToken = await jwt.sign(correctUser.dataValues.id);
+
+        return res.status(200).json({
+            user: authenticatedUser,
+            token: jwtToken,
+            correctUser:correctUser.dataValues
+        });
+    }
+
+    return UnauthorizedError('아이디와 비밀번호를 확인해 주세요');
+
 }
 
 
