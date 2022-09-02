c893167024c9754ad7e6de1fffe70ad33bf73167
leegwonseok <leestart1023@gmail.com>
2022-09-03 02:27:39 +0900

    Feat: Post API URI 라우터 틀 생성

--- components/posts/postApi.js
+++ components/posts/postApi.js
@@ -1,76 +1,172 @@
 const express = require('express');
 const postService = require('../posts/postService');
-
 const router = express.Router();
 
-module.exports = router;
+
+// async middleware
+const wrapAsyncCtrl = (fn) => {
+  return (req, res, next) => {
+    fn(req, res, next).catch(next)
+  }
+}
+
+// try catch Error handler
 
 /**
- * 예시: 게시글 삭제하기
- *
- * router.delete('/:id', async (req, res, next) => {
- *   const id = req.params.id;
- *   try {
- *     await postService.validateOpeningId(id);
- *
- *     await postService.delete(id);
- *
- *     res.status(200).json({
- *       success: true,
- *       message: `${id} was deleted successfully.`
- *     });
- *   } catch (e) {
- *     next(e);
- *   }
- * });
+ * @todo 운영게시판 CRUD
  */
 
-/**
- * @todo 운영게시판 게시글 작성하기
- */
+// 운영게시판 생성
+router.post('/posts/operation', isAdmin, async (req, res, next) => {
+  try {
+    // controller
+  } catch (err) {
+    next(err);
+  }
+})
+
+// 운영게시판 전체 조회 (페이지네이션 적용, 검색 적용)
+router.get('/posts/operation', isAdmin, async (req, res, next) => {
+  const { page } = req.query;
+  try {
+    // controller
+  } catch (err) {
+    next(err);
+  }
+})
+
+// 운영게시판 상세 조회
+router.get('/posts/operation/:postId', isAdmin, async (req, res, next) => {
+  const { postId } = req.params;
+  try {
+    // controller
+  } catch (err) {
+    next(err);
+  }
+})
+
+// 운영게시판 수정
+router.patch('/posts/operation/:postId', isAdmin, async (req, res, next) => {
+  const { postId } = req.params;
+  try {
+    // controller
+  } catch (err) {
+    next(err);
+  }
+})
+
+// 운영게시판 삭제
+router.get('/posts/operation/:postId', isAdmin, async (req, res, next) => {
+  const { postId } = req.params;
+  try {
+    // controller
+  } catch (err) {
+    next(err);
+  }
+})
 
 /**
- * @todo 운영게시판 게시글 모두 조회하기
+ * @todo 자유게시판 CRUD
  */
 
-/**
- * @todo 운영게시판 게시글 단건 조회하기
- */
+// 자유게시판 생성
+router.post('/posts/free', async (req, res, next) => {
+  try {
+    // controller
+  } catch (err) {
+    next(err);
+  }
+})
+
+// 자유게시판 전체 조회
+router.get('/posts/free', async (req, res, next) => {
+  try {
+    // controller
+  } catch (err) {
+    next(err);
+  }
+})
+
+// 자유게시판 상세 조회
+router.get('/posts/free/:postId', async (req, res, next) => {
+  const { postId } = req.params;
+  try {
+    // controller
+  } catch (err) {
+    next(err);
+  }
+})
+
+// 자유게시판 수정
+router.patch('/posts/free/:postId', async (req, res, next) => {
+  const { postId } = req.params;
+  try {
+    // controller
+  } catch (err) {
+    next(err);
+  }
+})
+
+// 자유게시판 삭제
+router.get('/posts/free/:postId', async (req, res, next) => {
+  const { postId } = req.params;
+  try {
+    // controller
+  } catch (err) {
+    next(err);
+  }
+})
 
 /**
- * @todo 운영게시판 게시글 삭제하기
+ * @todo 공지 CRUD
  */
 
-/**
- * @todo 공지사항 게시글 작성하기
- */
+// 공지 생성
+router.post('/posts/notice', isAdmin, async (req, res, next) => {
+  try {
+    // controller
+  } catch (err) {
+    next(err);
+  }
+})
 
-/**
- * @todo 공지사항 게시글 모두 조회하기
- */
+// 공지 전체 조회
+router.get('/posts/notice', async (req, res, next) => {
+  try {
+    // controller
+  } catch (err) {
+    next(err);
+  }
+})
 
-/**
- * @todo 공지사항 게시글 단건 조회하기
- */
+// 공지 상세 조회
+router.get('/posts/notice/:postId', async (req, res, next) => {
+  const { postId } = req.params;
+  try {
+    // controller
+  } catch (err) {
+    next(err);
+  }
+})
 
-/**
- * @todo 공지사항 게시글 삭제하기
- */
+// 공지 수정
+router.patch('/posts/notice/:postId', isAdmin, async (req, res, next) => {
+  const { postId } = req.params;
+  try {
+    // controller
+  } catch (err) {
+    next(err);
+  }
+})
 
-/**
- * @todo 자유게시판 게시글 작성하기
- */
-
-/**
- * @todo 자유게시판 게시글 모두 조회하기
- */
-
-/**
- * @todo 자유게시판 게시글 단건 조회하기
- */
-
-/**
- * @todo 자유게시판 게시글 삭제하기
- */
+// 공지 삭제
+router.get('/posts/notice/:postId', isAdmin, async (req, res, next) => {
+  const { postId } = req.params;
+  try {
+    // controller
+  } catch (err) {
+    next(err);
+  }
+})
 
 module.exports = router;
