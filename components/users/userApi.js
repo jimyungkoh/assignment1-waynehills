be3ef81273158c9844e5873fcf770656139f7a2f
seunam <seunam@student.42seoul.kr>
2022-09-03 00:13:07 +0900

    refactor: 상태코드 수정 및 주석 수정

--- components/users/userApi.js
+++ components/users/userApi.js
@@ -4,26 +4,7 @@ const userService = require("../users/userService");
 const router = express.Router();
 
 /**
- * 예시: 회원 삭제하기
- * router.delete('/:user_id', async (req, res, next) => {
- *   const user_id = req.params.user_id;
- *   try {
- *     await openingService.validateOpeningId(user_id);
- *
- *     await openingService.delete(user_id);
- *
- *     res.status(200).json({
- *       success: true,
- *       message: `${user_id} was deleted successfully.`
- *     });
- *   } catch (e) {
- *     next(e);
- *   }
- * });
- * */
-
-/**
- * @todo 회원 등록하기
+ * @description 회원 등록하기
  * */
 
 router.post("/signUp", async (req, res, next) => {
@@ -48,7 +29,7 @@ router.post("/signUp", async (req, res, next) => {
 });
 
 /***
- * @todo 회원 로그인 기능 구현
+ * @description 회원 로그인 기능 구현
  */
 
 router.post("/login", async (req, res, next) => {
@@ -66,7 +47,7 @@ router.post("/login", async (req, res, next) => {
 });
 
 /**
- * @todo 회원 삭제하기
+ * @description 회원 삭제하기
  * */
 
 router.delete("/delete", async (req, res, next) => {
@@ -75,7 +56,7 @@ router.delete("/delete", async (req, res, next) => {
   try {
     await userService.deleteUser(username);
 
-    res.status(201).json({
+    res.status(200).json({
       success: true,
       message: `회원탈퇴가 완료되었습니다.`,
     });
@@ -85,7 +66,7 @@ router.delete("/delete", async (req, res, next) => {
 });
 
 /**
- * @todo 회원 등급변경
+ * @description 회원 등급변경
  * */
 
 router.patch("/role", async (req, res, next) => {
@@ -94,7 +75,7 @@ router.patch("/role", async (req, res, next) => {
   try {
     await userService.editUserRole(username, role);
 
-    res.status(201).json({
+    res.status(200).json({
       success: true,
       message: `${username}의 회원 등급 변경이 완료되었습니다.`,
     });
