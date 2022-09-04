72f64e4d99ac7f3a0e712bbbdb1b58100bbce5d4
leegwonseok <leestart1023@gmail.com>
2022-09-04 18:49:02 +0900

    feat: 운영게시판 API 설계 진행

--- components/posts/postApi.js
+++ components/posts/postApi.js
@@ -1,172 +1,47 @@
 const express = require('express');
+const { PostModel } = require('../../model');
 const postService = require('../posts/postService');
 const router = express.Router();
-
+const postService = require('./postService');
 
 // async middleware
-const wrapAsyncCtrl = (fn) => {
+const wrapAsync = (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next)
   }
 }
 
-// try catch Error handler
 
 /**
- * @todo 운영게시판 CRUD
+ * @description 운영게시판 Create
+ * @param {String} 자원 URI 경로
+ * @param {wrapAsync} 트라이캐치 핸들러
  */
+router.post('/posts/operation', wrapAsync(postService.createPost));
 
-// 운영게시판 생성
-router.post('/posts/operation', isAdmin, async (req, res, next) => {
-  try {
-    // controller
-  } catch (err) {
-    next(err);
-  }
-})
+/**
+ * @description 운영게시판 Read(전체)
+ * @param {String} 자원 URI 경로
+ * @param {wrapAsync} 트라이캐치 핸들러
+ */
+router.get('/posts/operation', wrapAsync(postService.Controller));
+
+
+
+
+
 
 // 운영게시판 전체 조회 (페이지네이션 적용, 검색 적용)
-router.get('/posts/operation', isAdmin, async (req, res, next) => {
-  const { page } = req.query;
-  try {
-    // controller
-  } catch (err) {
-    next(err);
+// posts/operation?page=1&pageSize=5
+router.get('/posts/operation', pagiNation, wrapAsync(async (req, res, next) => {
+  const pageInfo = req.query;
+  if (!page) {
+    // 쿼리가 없는 경우
+  } else {
+    // 쿼리가 있는 경우
+    const page = parseInt(pageInfo.page);
+    const pageSize = parseInt(pageInfo.pageSize);
   }
-})
-
-// 운영게시판 상세 조회
-router.get('/posts/operation/:postId', isAdmin, async (req, res, next) => {
-  const { postId } = req.params;
-  try {
-    // controller
-  } catch (err) {
-    next(err);
-  }
-})
-
-// 운영게시판 수정
-router.patch('/posts/operation/:postId', isAdmin, async (req, res, next) => {
-  const { postId } = req.params;
-  try {
-    // controller
-  } catch (err) {
-    next(err);
-  }
-})
-
-// 운영게시판 삭제
-router.get('/posts/operation/:postId', isAdmin, async (req, res, next) => {
-  const { postId } = req.params;
-  try {
-    // controller
-  } catch (err) {
-    next(err);
-  }
-})
-
-/**
- * @todo 자유게시판 CRUD
- */
-
-// 자유게시판 생성
-router.post('/posts/free', async (req, res, next) => {
-  try {
-    // controller
-  } catch (err) {
-    next(err);
-  }
-})
-
-// 자유게시판 전체 조회
-router.get('/posts/free', async (req, res, next) => {
-  try {
-    // controller
-  } catch (err) {
-    next(err);
-  }
-})
-
-// 자유게시판 상세 조회
-router.get('/posts/free/:postId', async (req, res, next) => {
-  const { postId } = req.params;
-  try {
-    // controller
-  } catch (err) {
-    next(err);
-  }
-})
-
-// 자유게시판 수정
-router.patch('/posts/free/:postId', async (req, res, next) => {
-  const { postId } = req.params;
-  try {
-    // controller
-  } catch (err) {
-    next(err);
-  }
-})
-
-// 자유게시판 삭제
-router.get('/posts/free/:postId', async (req, res, next) => {
-  const { postId } = req.params;
-  try {
-    // controller
-  } catch (err) {
-    next(err);
-  }
-})
-
-/**
- * @todo 공지 CRUD
- */
-
-// 공지 생성
-router.post('/posts/notice', isAdmin, async (req, res, next) => {
-  try {
-    // controller
-  } catch (err) {
-    next(err);
-  }
-})
-
-// 공지 전체 조회
-router.get('/posts/notice', async (req, res, next) => {
-  try {
-    // controller
-  } catch (err) {
-    next(err);
-  }
-})
-
-// 공지 상세 조회
-router.get('/posts/notice/:postId', async (req, res, next) => {
-  const { postId } = req.params;
-  try {
-    // controller
-  } catch (err) {
-    next(err);
-  }
-})
-
-// 공지 수정
-router.patch('/posts/notice/:postId', isAdmin, async (req, res, next) => {
-  const { postId } = req.params;
-  try {
-    // controller
-  } catch (err) {
-    next(err);
-  }
-})
-
-// 공지 삭제
-router.get('/posts/notice/:postId', isAdmin, async (req, res, next) => {
-  const { postId } = req.params;
-  try {
-    // controller
-  } catch (err) {
-    next(err);
-  }
-})
+}))
 
 module.exports = router;
