11d1db1052f1580e96b659403fc27d4d0a58e2e5
leegwonseok <leestart1023@gmail.com>
2022-09-05 01:12:21 +0900

    feat: Post API 설계 전체 진행

--- components/posts/postApi.js
+++ components/posts/postApi.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const { PostModel } = require('../../model');
-const postService = require('../posts/postService');
 const router = express.Router();
 const postService = require('./postService');
 
@@ -13,19 +12,81 @@ const wrapAsync = (fn) => {
 
 
 /**
- * @description 운영게시판 Create
- * @param {String} 자원 URI 경로
- * @param {wrapAsync} 트라이캐치 핸들러
+ * @description 운영게시판 생성(Create)
  */
 router.post('/posts/operation', wrapAsync(postService.createPost));
 
 /**
- * @description 운영게시판 Read(전체)
- * @param {String} 자원 URI 경로
- * @param {wrapAsync} 트라이캐치 핸들러
+ * @description 운영게시판 전체 조회(Read)
  */
-router.get('/posts/operation', wrapAsync(postService.Controller));
+router.get('/posts/operation', wrapAsync(postService.readPosts));
 
+/**
+ * @description 운영게시판 상세 조회(Read)
+ */
+router.get('/posts/operation/:postId', wrapAsync(postService.readPost));
+
+/**
+ * @description 운영게시판 수정(Update)
+ */
+router.get('/posts/operation/:postId', wrapAsync(postService.updatePost));
+
+/**
+ * @description 운영게시판 삭제(Delete)
+ */
+router.get('/posts/operation/:postId', wrapAsync(postService.deletePost));
+
+
+/**
+ * @description 자유게시판 생성(Create)
+ */
+router.post('/posts/free', wrapAsync(postService.createPost));
+
+/**
+ * @description 자유게시판 전체 조회(Read)
+ */
+router.get('/posts/free', wrapAsync(postService.readPosts));
+
+/**
+ * @description 자유게시판 상세 조회(Read)
+ */
+router.get('/posts/free/:postId', wrapAsync(postService.readPost));
+
+/**
+ * @description 자유게시판 수정(Update)
+ */
+router.get('/posts/free/:postId', wrapAsync(postService.updatePost));
+
+/**
+ * @description 자유게시판 삭제(Delete)
+ */
+router.get('/posts/free/:postId', wrapAsync(postService.deletePost));
+
+
+/**
+ * @description 공지 생성(Create)
+ */
+router.post('/posts/notice', wrapAsync(postService.createPost));
+
+/**
+ * @description 공지 전체 조회(Read)
+ */
+router.get('/posts/notice', wrapAsync(postService.readPosts));
+
+/**
+ * @description 공지 상세 조회(Read)
+ */
+router.get('/posts/notice/:postId', wrapAsync(postService.readPost));
+
+/**
+ * @description 공지 수정(Update)
+ */
+router.get('/posts/notice/:postId', wrapAsync(postService.updatePost));
+
+/**
+ * @description 공지 삭제(Delete)
+ */
+router.get('/posts/notice/:postId', wrapAsync(postService.deletePost));
 
 
 
