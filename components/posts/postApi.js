208d0bfe6a987b1a6293b1e9f68a20fc8a5e1712
leegwonseok <leestart1023@gmail.com>
2022-09-05 18:40:37 +0900

    feat: 운영 게시판 API 설계 진행

--- components/posts/postApi.js
+++ components/posts/postApi.js
@@ -3,111 +3,103 @@ const router = express.Router();
 const postService = require('./postService');
 
 // async middleware
-const wrapAsync = (fn) => {
-  return (req, res, next) => {
-    fn(req, res, next).catch(next)
-  }
-}
+// const wrapAsync = (fn) => {
+//   return (req, res, next) => {
+//     fn(req, res, next).catch(next)
+//   }
+// }
 
 /**
  * @description 운영게시판 생성(Create)
+ * @description POST /posts/operation
  */
-router.get('operation/:postId', async (req, res, next) => {
+router.post('/operation', async (req, res, next) => {
+  const { title, content, type } = req.body;
+  try {
+    await postService.createPost({
+      title,
+      content,
+      type,
+    }, {
+      user_id: user.id,
+    });
+    res.status(201).json({
+      success: true,
+      message: `운영게시판 생성이 완료되었습니다.`,
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
+/**
+ * @description 운영게시판 전체 조회(Read)
+ * @description GET /posts/operation?skip=1&limit=10
+ */
+router.get('/operation', async (req, res, next) => {
+  const { skip, limit, title } = req.query;
+  const postType = 'operation';
+  try {
+    if (!title) {
+      // 검색 X
+      await postService.readPostsByType(user, postType, skip, limit)
+      .then(result => {
+        return res.status(200).json(result);
+      });  
+    } 
+    // 검색 O
+
+  } catch (err) {
+    next(err);
+  }
+});
+
+/**
+ * @description 운영게시판 상세 조회(Read)
+ * @description GET /posts/operation/{postId}
+ */
+router.patch('/operation/:postId', async (req, res, next) => {
   const { postId } = req.params;
   try {
-    await postService.readPost(postId, userRoleAdmin);
-  } catch (e) {
-    next(e);
+    await postService.readPost(postId, user)
+      .then(result => {
+        res.status(200).json(result);
+      });
+  } catch (err) {
+    next(err);
   }
-})
+});
 
-// /**
-//  * @description 운영게시판 전체 조회(Read)
-//  */
-// router.get('/posts/operation', wrapAsync(postService.readPosts));
+/**
+ * @description 운영게시판 수정(Update)
+ * @description PATCH /posts/operation/{postId}
+ */
+router.get('/operation/:postId', async (req, res, next) => {
+  const { postId } = req.params;
+  try {
+    await postService.readPost(postId, user)
+      .then(result => {
+        res.status(200).send(`'${postId}'번 글이 수정되었습니다.`);
+      });
+  } catch (err) {
+    next(err);
+  }
+});
 
-// /**
-//  * @description 운영게시판 상세 조회(Read)
-//  */
-// router.get('/posts/operation/:postId', wrapAsync(postService.readPost));
-
-// /**
-//  * @description 운영게시판 수정(Update)
-//  */
-// router.get('/posts/operation/:postId', wrapAsync(postService.updatePost));
-
-// /**
-//  * @description 운영게시판 삭제(Delete)
-//  */
-// router.get('/posts/operation/:postId', wrapAsync(postService.deletePost));
-
-
-// /**
-//  * @description 자유게시판 생성(Create)
-//  */
-// router.post('/posts/free', wrapAsync(postService.createPost));
-
-// /**
-//  * @description 자유게시판 전체 조회(Read)
-//  */
-// router.get('/posts/free', wrapAsync(postService.readPosts));
-
-// /**
-//  * @description 자유게시판 상세 조회(Read)
-//  */
-// router.get('/posts/free/:postId', wrapAsync(postService.readPost));
-
-// /**
-//  * @description 자유게시판 수정(Update)
-//  */
-// router.get('/posts/free/:postId', wrapAsync(postService.updatePost));
-
-// /**
-//  * @description 자유게시판 삭제(Delete)
-//  */
-// router.get('/posts/free/:postId', wrapAsync(postService.deletePost));
-
-
-// /**
-//  * @description 공지 생성(Create)
-//  */
-// router.post('/posts/notice', wrapAsync(postService.createPost));
-
-// /**
-//  * @description 공지 전체 조회(Read)
-//  */
-// router.get('/posts/notice', wrapAsync(postService.readPosts));
-
-// /**
-//  * @description 공지 상세 조회(Read)
-//  */
-// router.get('/posts/notice/:postId', wrapAsync(postService.readPost));
-
-// /**
-//  * @description 공지 수정(Update)
-//  */
-// router.get('/posts/notice/:postId', wrapAsync(postService.updatePost));
-
-// /**
-//  * @description 공지 삭제(Delete)
-//  */
-// router.get('/posts/notice/:postId', wrapAsync(postService.deletePost));
-
-
-
-
-
-// 운영게시판 전체 조회 (페이지네이션 적용, 검색 적용)
-// posts/operation?page=1&pageSize=5
-// router.get('/posts/operation', pagiNation, wrapAsync(async (req, res, next) => {
-//   const pageInfo = req.query;
-//   if (!page) {
-//     // 쿼리가 없는 경우
-//   } else {
-//     // 쿼리가 있는 경우
-//     const page = parseInt(pageInfo.page);
-//     const pageSize = parseInt(pageInfo.pageSize);
-//   }
-// }))
+/**
+ * @description 운영게시판 삭제(Delete)
+ * @description DELETE /posts/operation/{postId}
+ */
+router.delete('/operation/:postId', async (req, res, next) => {
+  const { postId } = req.params;
+  try {
+    await postService.readPost(postId, user)
+      .then(result => {
+        res.status(200).send(`'${postId}'번 글이 삭제되었습니다.`);
+      });
+  } catch (err) {
+    next(err);
+  }
+});
 
 module.exports = router;
