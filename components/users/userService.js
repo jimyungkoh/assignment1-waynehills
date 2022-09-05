c0dc3572013f95934827d4455d4966dba9e9c7a4
seunam <seunam@student.42seoul.kr>
2022-09-05 15:45:38 +0900

    refactor: 불필요한 객체화 제거

--- components/users/userService.js
+++ components/users/userService.js
@@ -94,7 +94,6 @@ exports.login = async (username, password) => {
       jwtConfig.secretKey,
       jwtConfig.option
     );
-
     return jwtToken;
   } else {
     throw new BadRequestError("cant find user");
@@ -132,10 +131,8 @@ exports.editUserRole = async (username, role) => {
         username: username,
       },
     }
-
   );
-
-
+};
 
 /**
  * @todo 회원 중복아이디 확인 validateUserId 메서드
