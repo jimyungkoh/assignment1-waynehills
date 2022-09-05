644698d04aeecacf2ba1abbc81691eadaaefd35e
leegwonseok <leestart1023@gmail.com>
2022-09-05 22:00:35 +0900

    feat: POST API 설계 진행

--- components/posts/postApi.js
+++ components/posts/postApi.js
@@ -7,8 +7,8 @@ const { userAuthChecker } = require('../../middlewares/userAuthChecker');
  * @description 운영게시판 생성(Create)
  * @description POST /posts/operation
  */
-router.post('/operation', async (req, res, next) => {
-  const { title, content, type } = req.body;
+router.post('/operation', userAuthChecker(["user"]), async (req, res, next) => {
+  const { title, content, type, user } = req.body;
   try {
     await postService.createPost({
       title,
@@ -28,11 +28,11 @@ router.post('/operation', async (req, res, next) => {
  * @description 운영게시판 전체 조회(Read)
  * @description GET /posts/operation?skip=1&limit=10
  */
-router.get('/operation', async (req, res, next) => {
+router.get('/operation', userAuthChecker(["admin", "user"]), async (req, res, next) => {
   const { skip, limit } = req.query;
+  const { user } = req.body;
   const postType = 'operation';
   try {
-    // 검색 X
     await postService.readPostsByType(user, postType, skip, limit)
       .then(result => {
         res.status(200).json(result);
@@ -42,13 +42,13 @@ router.get('/operation', async (req, res, next) => {
   }
 });
 
-
 /**
  * @description 운영게시판 상세 조회(Read)
  * @description GET /posts/operation/{postId}
  */
-router.patch('/operation/:postId', async (req, res, next) => {
+router.patch('/operation/:postId', userAuthChecker(["admin"]), async (req, res, next) => {
   const { postId } = req.params;
+  const { user } = req.body;
   try {
     await postService.readPost(postId, user)
       .then(result => {
@@ -63,8 +63,9 @@ router.patch('/operation/:postId', async (req, res, next) => {
  * @description 운영게시판 수정(Update)
  * @description PATCH /posts/operation/{postId}
  */
-router.get('/operation/:postId', async (req, res, next) => {
+router.get('/operation/:postId', userAuthChecker(["admin"]), async (req, res, next) => {
   const { postId } = req.params;
+  const { user } = req.body;
   try {
     await postService.readPost(postId, user)
       .then(result => {
@@ -79,8 +80,191 @@ router.get('/operation/:postId', async (req, res, next) => {
  * @description 운영게시판 삭제(Delete)
  * @description DELETE /posts/operation/{postId}
  */
-router.delete('/operation/:postId', async (req, res, next) => {
+router.delete('/operation/:postId', userAuthChecker(["admin"]), async (req, res, next) => {
   const { postId } = req.params;
+  const { user } = req.body;
+  try {
+    await postService.readPost(postId, user)
+      .then(result => {
+        res.status(200).send(`'${postId}'번 글이 삭제되었습니다.`);
+      });
+  } catch (err) {
+    next(err);
+  }
+});
+
+
+/**
+ * @description 자유게시판 생성(Create)
+ * @description POST /posts/free
+ */
+router.post('/free', userAuthChecker(["admin", "user"]), async (req, res, next) => {
+  const { title, content, type, user } = req.body;
+  try {
+    await postService.createPost({
+      title,
+      content,
+      type,
+    }, user);
+    res.status(201).json({
+      success: true,
+      message: `운영게시판 생성이 완료되었습니다.`,
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
+/**
+ * @description 자유게시판 전체 조회(Read)
+ * @description GET /posts/free?skip=1&limit=10
+ */
+router.get('/free', userAuthChecker(["admin", "user"]), async (req, res, next) => {
+  const { skip, limit } = req.query;
+  const { user } = req.body;
+  const postType = 'operation';
+  try {
+    await postService.readPostsByType(user, postType, skip, limit)
+      .then(result => {
+        res.status(200).json(result);
+      }); 
+  } catch (err) {
+    next(err);
+  }
+});
+
+/**
+ * @description 자유게시판 상세 조회(Read)
+ * @description GET /posts/free/{postId}
+ */
+router.patch('/free/:postId', userAuthChecker(["admin"]), async (req, res, next) => {
+  const { postId } = req.params;
+  const { user } = req.body;
+  try {
+    await postService.readPost(postId, user)
+      .then(result => {
+        res.status(200).json(result);
+      });
+  } catch (err) {
+    next(err);
+  }
+});
+
+/**
+ * @description 자유게시판 수정(Update)
+ * @description PATCH /posts/free/{postId}
+ */
+router.get('/free/:postId', userAuthChecker(["admin"]), async (req, res, next) => {
+  const { postId } = req.params;
+  const { user } = req.body;
+  try {
+    await postService.readPost(postId, user)
+      .then(result => {
+        res.status(200).send(`'${postId}'번 글이 수정되었습니다.`);
+      });
+  } catch (err) {
+    next(err);
+  }
+});
+
+/**
+ * @description 자유게시판 삭제(Delete)
+ * @description DELETE /posts/free/{postId}
+ */
+router.delete('/free/:postId', userAuthChecker(["admin"]), async (req, res, next) => {
+  const { postId } = req.params;
+  const { user } = req.body;
+  try {
+    await postService.readPost(postId, user)
+      .then(result => {
+        res.status(200).send(`'${postId}'번 글이 삭제되었습니다.`);
+      });
+  } catch (err) {
+    next(err);
+  }
+});
+
+
+/**
+ * @description 공지 생성(Create)
+ * @description POST /posts/notice
+ */
+router.post('/notice', userAuthChecker(["admin", "user"]), async (req, res, next) => {
+  const { title, content, type, user } = req.body;
+  try {
+    await postService.createPost({
+      title,
+      content,
+      type,
+    }, user);
+    res.status(201).json({
+      success: true,
+      message: `운영게시판 생성이 완료되었습니다.`,
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
+/**
+ * @description 공지 전체 조회(Read)
+ * @description GET /posts/notice?skip=1&limit=10
+ */
+router.get('/notice', userAuthChecker(["admin", "user"]), async (req, res, next) => {
+  const { skip, limit } = req.query;
+  const { user } = req.body;
+  const postType = 'operation';
+  try {
+    await postService.readPostsByType(user, postType, skip, limit)
+      .then(result => {
+        res.status(200).json(result);
+      }); 
+  } catch (err) {
+    next(err);
+  }
+});
+
+/**
+ * @description 공지 상세 조회(Read)
+ * @description GET /posts/notice/{postId}
+ */
+router.patch('/notice/:postId', userAuthChecker(["admin"]), async (req, res, next) => {
+  const { postId } = req.params;
+  const { user } = req.body;
+  try {
+    await postService.readPost(postId, user)
+      .then(result => {
+        res.status(200).json(result);
+      });
+  } catch (err) {
+    next(err);
+  }
+});
+
+/**
+ * @description 공지 수정(Update)
+ * @description PATCH /posts/notice/{postId}
+ */
+router.get('/notice/:postId', userAuthChecker(["admin"]), async (req, res, next) => {
+  const { postId } = req.params;
+  const { user } = req.body;
+  try {
+    await postService.readPost(postId, user)
+      .then(result => {
+        res.status(200).send(`'${postId}'번 글이 수정되었습니다.`);
+      });
+  } catch (err) {
+    next(err);
+  }
+});
+
+/**
+ * @description 공지 삭제(Delete)
+ * @description DELETE /posts/notice/{postId}
+ */
+router.delete('/notice/:postId', userAuthChecker(["admin"]), async (req, res, next) => {
+  const { postId } = req.params;
+  const { user } = req.body;
   try {
     await postService.readPost(postId, user)
       .then(result => {
